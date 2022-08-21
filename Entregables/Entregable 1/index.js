e87867a85d256c0b034d040b8d6d70fdc1fe130e
JuanCruz <juanlimacaramico@gmail.com>
2022-08-20 23:08:12 -0300

    entregable 1 hecho

--- Entregables/Entregable 1/index.js
+++ Entregables/Entregable 1/index.js
@@ -1,12 +1,22 @@
-//condicional que analiza si somos tocayos.
-let nombre1 = prompt("ingrese nombre jugador 1:")
-let nombre2 = prompt("ingrese nombre jugador 2:")
-if(nombre1 === "" || nombre2 === ""){
-    alert("algun nombre es invalido")
-} else if((nombre1 !="") && nombre1.toLowerCase() === nombre2.toLowerCase()){
-        alert(`hola jugador ${nombre1}`)
-        alert(`hola jugador ${nombre2}`)
-        alert('ustedes son tocayos!')
-}else if(nombre1.toLowerCase() != nombre2.toLowerCase()){
-        alert('no somos tocayos!')
+let input = prompt("Ingrese nombre:").toUpperCase();
+while(input === ""){
+    alert("nombre invalido")
+    input = prompt("Ingrese nombre:").toUpperCase();
+}
+
+    alert(`Hola ${input}`)
+alert("Cuales son tus 5 gustos de helados preferidos?")
+let gustos = [];   
+
+if(input=!""){        
+    
+    for(i=0; i<5; i++){
+        let gusto = prompt("ingresa el gusto").toUpperCase();
+        gustos.push(gusto)
+        alert(`Elegiste: ${gusto}`); 
+    }  
+    
+    //let entry = prompt("ingrese un nombre");
     }
+alert(`tus gustos son: ${gustos}`);
+alert("Gracias por compartirme tus gustos de helado")
