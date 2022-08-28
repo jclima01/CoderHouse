61c93e6b038739279590645039f77d403ff86d43
JuanCruz <juanlimacaramico@gmail.com>
2022-08-28 20:27:09 -0300

    Entrega completa

--- Entregables/entregable 2/index.js
+++ Entregables/entregable 2/index.js
@@ -1,5 +1,36 @@
-function saludar(nombre){
-    console.log("Hola: ", nombre)
+alert("Bienvenido al simulador de prestamos")
+let simulador = function () {
+
+    let ingreso = prompt("Acepta las condiciones para ingreasr al simulador? si o no").toLowerCase()
+    if (ingreso === "si") {
+        let calcularPrecioCuota = function () {
+            let montoPedido = parseInt(prompt("ingrese el monto solicitado"))
+            let cantidadDeCoutas = parseInt(prompt("ingrese la cantidad de cuotas, puede elegir entre 3, 6 o 12"))
+            
+            if (cantidadDeCoutas === 3) {
+                montoPedido = montoPedido * 1.30;
+                alert(`monto con interes: ${montoPedido}`)
+                precioCuota = montoPedido / 3;
+                alert(`El precio de su cuota es ${precioCuota}`);
+            } else if (cantidadDeCoutas === 6) {
+                montoPedido = montoPedido * 1.35;
+                alert(`monto con interes: ${montoPedido}`)
+                precioCuota = montoPedido / 6
+                alert(`El precio de su cuota es ${precioCuota}`);
+            } else if (cantidadDeCoutas === 12) {
+                montoPedido = montoPedido * 1.45;
+                alert(`monto con interes: ${montoPedido}`)
+                precioCuota = montoPedido / 12;
+                alert(`El precio de su cuota es ${precioCuota}`);
+            } else {
+                alert("ingreso cuotas invalidas.");
+            }
+
+        }
+        calcularPrecioCuota()
+    } else {
+        alert("no acepto las condiciones")
+    }
 }
 
-saludar("Juan")
+simulador()
