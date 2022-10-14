00b6977e58e05c14e3e3230c51a19d3b082757cf
JuanCruz <juanlimacaramico@gmail.com>
2022-10-13 21:53:05 -0300

    fetch

--- practica/index.js
+++ practica/index.js
@@ -1,42 +1,37 @@
-class Producto {
-    constructor(id, nombre, precio, descripcion, imagen) {
-        this.id = id;
-        this.nombre = nombre;
-        this.precio = precio;
-        this.descripcion = descripcion;
-        this.imagen = imagen;
+// class Producto {
+//     constructor(id, nombre, precio, descripcion, imagen) {
+//         this.id = id;
+//         this.nombre = nombre;
+//         this.precio = precio;
+//         this.descripcion = descripcion;
+//         this.imagen = imagen;
 
-    }
-}
+//     }
+// }
 
-let productsDB = [new Producto(1, 'Calabresa', 1950, 'Pizza de Muzzarella, Longaniza Calabresa, Morrones, Salsa de Tomate, Condimentos y Aceitunas Verdes.', 'https://i.ibb.co/27zLq3m/Calabresa.png'),
-new Producto(2, 'Especial', 2000, "Pizza de Muzzarella, Jamon Natural, Morrones , Salsa de Tomate, Condimentos y Aceitunas Verdes.", 'https://i.ibb.co/5cn3fQW/Especial.png'),
-new Producto(3, 'Fugazza', 1050, 'Pizza de Cebolla, Condimentos y Aceitunas Verdes. (sin Quesos)', 'https://i.ibb.co/MV4TzFG/Fugazza.png'),
-new Producto(4, 'Muzza y jamon', 1950, 'Pizza de Muzzarella, Jamon Natural, Salsa de Tomate, Condimentos y Aceitunas Verdes.', 'https://i.ibb.co/Dz9H9kr/Muzza-y-jamon.png'),
-new Producto(5, 'Muzza y Morron', 1950, 'Pizza de Muzzarella, Morrones, Salsa de Tomate, Condimentos y Aceitunas Verdes.', 'https://i.ibb.co/Qjn7RSP/Muzza-y-Morron.png'),
-new Producto(6, 'Muzzarella', 1950, 'Pizza de Muzzarella, Salsa de Tomate, Condimentos y Aceitunas Verdes.', 'https://i.ibb.co/x8XQjc6/Muzzarella.png')
-]
+// let productsDB = [new Producto(1, 'Calabresa', 1950, 'Pizza de Muzzarella, Longaniza Calabresa, Morrones, Salsa de Tomate, Condimentos y Aceitunas Verdes.', 'https://i.ibb.co/27zLq3m/Calabresa.png'),
+// new Producto(2, 'Especial', 2000, "Pizza de Muzzarella, Jamon Natural, Morrones , Salsa de Tomate, Condimentos y Aceitunas Verdes.", 'https://i.ibb.co/5cn3fQW/Especial.png'),
+// new Producto(3, 'Fugazza', 1050, 'Pizza de Cebolla, Condimentos y Aceitunas Verdes. (sin Quesos)', 'https://i.ibb.co/MV4TzFG/Fugazza.png'),
+// new Producto(4, 'Muzza y jamon', 1950, 'Pizza de Muzzarella, Jamon Natural, Salsa de Tomate, Condimentos y Aceitunas Verdes.', 'https://i.ibb.co/Dz9H9kr/Muzza-y-jamon.png'),
+// new Producto(5, 'Muzza y Morron', 1950, 'Pizza de Muzzarella, Morrones, Salsa de Tomate, Condimentos y Aceitunas Verdes.', 'https://i.ibb.co/Qjn7RSP/Muzza-y-Morron.png'),
+// new Producto(6, 'Muzzarella', 1950, 'Pizza de Muzzarella, Salsa de Tomate, Condimentos y Aceitunas Verdes.', 'https://i.ibb.co/x8XQjc6/Muzzarella.png')
+// ]
+// console.log(JSON.stringify(productsDB))
+
 let url = 'https://633ef66c83f50e9ba3bcfbe2.mockapi.io/Api'
-
-function postEnApi() {
-        productsDB.forEach(producto =>{
-            fetch(url,
-                {
-                    method: 'POST',
-                    body:JSON.stringify(producto) ,
-                    headers: {
-                      'Content-type': 'application/json; charset=UTF-8',
-                     },
-                }
-            )
-            .then(response => response.json())
-            .then(data => console.log(data))
-            
-            
-        })
-            
-    };
-postEnApi()
+let productsDB = []
+let cargarDatos = async() =>{
+    await fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        
+        productsDB = [...data]
+        console.log(productsDB)
+        guardarProductosStorage()
+    })
+}
+cargarDatos()
+renderizarProductos();
 
 let carrito = [];
 const items = document.querySelector("#items");
@@ -85,11 +80,11 @@ function renderizarProductos() {
         productoHTML = `
         <div class="col-12 col-md-4 nb-5 d-flex p-1 justify-content-center">
         <div class="card text-light bg-dark" style="width: 18rem;">
-            <img class="card-Img-top" src="${prod.imagen}" alt="card image cap">
+            <img class="card-Img-top" src="${prod?.imagen}" alt="card image cap">
             <div class="card-body">
-                <h5 class="card-title">${prod.nombre}</h5>
-                <p class="card-text">${prod.descripcion}</p>
-                <p>$${prod.precio}
+                <h5 class="card-title">${prod?.nombre}</h5>
+                <p class="card-text">${prod?.descripcion}</p>
+                <p>$${prod?.precio}
                 <button class="btn btn-primary" onclick="agregarProductoAlCarrito(${prod.id})">Añadir al carrito</button>
             </div>
             </div> 
@@ -120,9 +115,9 @@ function renderizarCarrito() {
         <div class="card w-50 p-1 >
             
             <div class="card-body p-1">
-                <img class="card-Img-top" src="${prod.imagen}" alt="card image cap">
-                <h5 class="card-title">${prod.nombre}</h5>
-                <p class="card-text">Cantidad: ${prod.cantidad}</p>
+                <img class="card-Img-top" src="${prod?.imagen}" alt="card image cap">
+                <h5 class="card-title">${prod?.nombre}</h5>
+                <p class="card-text">Cantidad: ${prod?.cantidad}</p>
                 <button class="btn btn-danger" onclick="eliminarProductoDelCarrito(${id})">Eliminar</button>
             </div>
         </div>
@@ -182,20 +177,21 @@ function iniciarChat(){
     let apellidoUsuario = document.getElementById("apellido-usuario").value;
     let telefonoUsuario = document.getElementById("telefono-usuario").value;
     
-    let mensaje = ` Nombre: ${nombreUsuario}
-                    Apellido: ${apellidoUsuario}
-                    Telefono: ${telefonoUsuario}
-                    Su pedido es:`
+    let mensaje = `Nombre: ${nombreUsuario}
+                    %0AApellido: ${apellidoUsuario}
+                    %0ATeléfono: ${telefonoUsuario}
+                    %0ADirección: ${direccion}
+                    %0ASu pedido es:`
     carrito.forEach(producto => {
         
-        mensaje += `*${producto.nombre} - ${producto.cantidad} U, 
+        mensaje += `%0A*${producto.nombre} - ${producto.cantidad}, 
         `;
     });
-    mensaje += `Dirección: ${direccion} Total: ${total.textContent}`
+    mensaje += `%0ATotal: ${total.textContent}`
 
     
    
-    botonEnviar.href = `https://wa.me/5492235304745?text=${mensaje}`
+    botonEnviar.href = `https://wa.me/5492236197018?text=${mensaje}`
     
     guardarCarritoStorage()
     
@@ -206,10 +202,14 @@ function eliminarProducto() {
     let nombre = document.getElementById("nombre-eliminar").value.toLowerCase()
     let producto = productsDB.find((producto) => producto.nombre.toLowerCase() === nombre)
     let productoIx  = productsDB.indexOf(producto)
+    console.log(productoIx)
+    console.log(producto.id)
    
     if (productoIx != -1) {
         productsDB.splice(productoIx, 1)
-
+        // fetch(`https://633ef66c83f50e9ba3bcfbe2.mockapi.io/Api/${productoIx}`, {
+        // method: 'DELETE',
+// });
         Swal.fire({
             position: 'top-end',
             icon: 'success',
@@ -230,7 +230,8 @@ function eliminarProducto() {
    
 }
 
-function agregarNuevosProducto() {
+async function agregarNuevosProducto() {
+    
     obtenerProductosStorage()
 
     let lastProduct = productsDB[productsDB.length-1]
@@ -244,25 +245,58 @@ function agregarNuevosProducto() {
             icon: 'error',
             title: 'Oops...',
             text: 'Producto ya existente',
-            footer: '<a href="">Ingrese un producto nuevo</a>'
+            footer: '<a>Ingrese un producto nuevo</a>'
         })
     } else {
-        productsDB.push(new Producto(
-            id,
-            nombre,
-            precio,
-            descripcion,
-            imagen
-        )
+parseInt()
+        
+        productsDB.push(
+            {id: parseInt(id),
+            nombre: nombre,
+            precio: precio,
+            descripcion: descripcion,
+            imagen: imagen}
         );
+        await fetch(url,
+            {
+                method: 'POST',
+                body:JSON.stringify(productsDB[productsDB.length - 1]) ,
+                headers: {
+                  'Content-type': 'application/json; charset=UTF-8',
+                 },
+            }
+        )
+        .then(response => response.json())
+        .then(data => console.log(data))
     }
+
+    
+    
 
     guardarProductosStorage()
     
 
 }
+//time
 
-
+const getTime = async () =>{
+    const date = document.getElementById("date");
+    fetch('http://worldtimeapi.org/api/timezone/America/Argentina/Buenos_Aires')
+    .then(response => response.json())
+    .then(data =>{
+        let date_time = data.datetime;
+        let time_zone = data.timezone;
+        let dayOfYear = data.day_of_year;
+        date.innerHTML =`
+            <li> Zona horaria: ${time_zone}</li>
+            <li> Fecha y hora: ${date_time}</li>
+            <li> Día del año: ${dayOfYear}</li>
+        `;
+    })
+    
+    
+}
+getTime();
 
 
 //listeners
